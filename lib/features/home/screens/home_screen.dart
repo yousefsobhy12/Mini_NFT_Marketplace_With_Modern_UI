b33c008101208eb35df89de6d91672bb7f96d414
yousefsobhy12 <yousefsobhy124@gmail.com>
2025-08-12 18:36:13 +0300

    feat: create category model and integrate it into category card with code refactor

--- lib/features/home/screens/home_screen.dart
+++ lib/features/home/screens/home_screen.dart
@@ -1,13 +1,20 @@
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
 import 'package:mini_nft_marketplace_with_modern_ui/core/resources/fonts_manager.dart';
 import 'package:mini_nft_marketplace_with_modern_ui/core/resources/strings_manager.dart';
+import 'package:mini_nft_marketplace_with_modern_ui/features/home/models/category_model.dart';
 import 'package:mini_nft_marketplace_with_modern_ui/features/home/widgets/custom_category_home_screen.dart';
 
+// ignore: must_be_immutable
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
-
+  HomeScreen({super.key});
+  List<CategoryModel> categories = [
+    CategoryModel(title: 'Music', image: 'assets/images/category_image3.png'),
+    CategoryModel(title: 'Art', image: 'assets/images/category_image1.png'),
+    CategoryModel(
+      title: 'Virtual Worlds',
+      image: 'assets/images/category_image2.png',
+    ),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,9 +29,25 @@ class HomeScreen extends StatelessWidget {
           ),
         ),
       ),
-      body: CustomCategoryCardHomeScreen(
-        title: 'Art',
-        image: 'assets/images/category_image1.png',
+      body: Column(
+        children: [
+          SizedBox(height: 20),
+          SizedBox(
+            height: 180,
+            child: ListView.separated(
+              itemCount: categories.length,
+              scrollDirection: Axis.horizontal,
+              itemBuilder: (context, index) {
+                return CustomCategoryCardHomeScreen(
+                  categoryModel: categories[index],
+                );
+              },
+              separatorBuilder: (context, index) {
+                return SizedBox(width: 10);
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
--- lib/features/home/widgets/custom_category_home_screen.dart
+++ lib/features/home/widgets/custom_category_home_screen.dart
@@ -1,61 +1,60 @@
 import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:mini_nft_marketplace_with_modern_ui/core/resources/fonts_manager.dart';
+import 'package:mini_nft_marketplace_with_modern_ui/features/home/models/category_model.dart';
 
 class CustomCategoryCardHomeScreen extends StatelessWidget {
   const CustomCategoryCardHomeScreen({
-    super.key,
-    required this.title,
-    required this.image,
+    super.key, required this.categoryModel,
   });
-  final String title;
-  final String image;
+  final CategoryModel categoryModel;
   @override
   Widget build(BuildContext context) {
-    return ClipRRect(
-      borderRadius: BorderRadius.circular(26),
-      child: Stack(
-        children: [
-          // Background image
-          Image.asset(image, width: 280, height: 180, fit: BoxFit.cover),
-          Positioned(
-            bottom: 0,
-            left: 0,
-            right: 0,
-            child: ClipRRect(
-              borderRadius: BorderRadiusGeometry.only(
-                topLeft: Radius.circular(8),
-                topRight: Radius.circular(8),
-              ),
-              child: BackdropFilter(
-                filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
-                child: Container(
-                  height: 55,
-                  decoration: BoxDecoration(
-                    gradient: LinearGradient(
-                      begin: Alignment.topCenter,
-                      end: Alignment.bottomCenter,
-                      colors: [
-                        Colors.transparent,
-                        Colors.transparent.withOpacity(0.3),
-                      ],
-                      stops: [0, 1],
+    return SizedBox(
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(26),
+        child: Stack(
+          children: [
+            Image.asset(categoryModel.image, width: 280, height: 180, fit: BoxFit.cover),
+            Positioned(
+              bottom: 0,
+              left: 0,
+              right: 0,
+              child: ClipRRect(
+                borderRadius: BorderRadiusGeometry.only(
+                  topLeft: Radius.circular(8),
+                  topRight: Radius.circular(8),
+                ),
+                child: BackdropFilter(
+                  filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+                  child: Container(
+                    height: 55,
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                        begin: Alignment.topCenter,
+                        end: Alignment.bottomCenter,
+                        colors: [
+                          Colors.transparent,
+                          Colors.transparent.withOpacity(0.3),
+                        ],
+                        stops: [0, 1],
+                      ),
                     ),
-                  ),
-                  alignment: Alignment.center,
-                  child: Text(
-                    title,
-                    style: const TextStyle(
-                      fontSize: 20,
-                      fontFamily: FontsManager.sfProDisplay,
-                      fontWeight: FontWeight.w900,
+                    alignment: Alignment.center,
+                    child: Text(
+                      categoryModel.title,
+                      style: const TextStyle(
+                        fontSize: 20,
+                        fontFamily: FontsManager.sfProDisplay,
+                        fontWeight: FontWeight.w900,
+                      ),
                     ),
                   ),
                 ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
