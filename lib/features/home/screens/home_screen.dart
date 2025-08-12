1452cea48960012e9788e3bc2bf805cf161b0462
yousefsobhy12 <yousefsobhy124@gmail.com>
2025-08-12 17:37:16 +0300

    feat: refactor home screen and implement custom category card

--- lib/features/home/screens/home_screen.dart
+++ lib/features/home/screens/home_screen.dart
@@ -3,6 +3,7 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:mini_nft_marketplace_with_modern_ui/core/resources/fonts_manager.dart';
 import 'package:mini_nft_marketplace_with_modern_ui/core/resources/strings_manager.dart';
+import 'package:mini_nft_marketplace_with_modern_ui/features/home/widgets/custom_category_home_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -21,56 +22,9 @@ class HomeScreen extends StatelessWidget {
           ),
         ),
       ),
-      body: ClipRRect(
-        borderRadius: BorderRadius.circular(26),
-        child: Stack(
-          children: [
-            // Background image
-            Image.asset(
-              'assets/images/category_image1.png',
-              width: 280,
-              height: 180,
-              fit: BoxFit.cover,
-            ),
-            Positioned(
-              bottom: 0,
-              left: 0,
-              right: 0,
-              child: ClipRRect(
-                borderRadius: BorderRadiusGeometry.only(
-                  topLeft: Radius.circular(8),
-                  topRight: Radius.circular(8),
-                ),
-                child: BackdropFilter(
-                  filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
-                  child: Container(
-                    height: 55,
-                    decoration: BoxDecoration(
-                      gradient: LinearGradient(
-                        begin: Alignment.topCenter,
-                        end: Alignment.bottomCenter,
-                        colors: [
-                          Colors.transparent,
-                          Colors.transparent.withOpacity(0.3),
-                        ],
-                        stops: [0, 1],
-                      ),
-                    ),
-                    alignment: Alignment.center,
-                    child: Text(
-                      'Art',
-                      style: const TextStyle(
-                        fontSize: 20,
-                        fontFamily: FontsManager.sfProDisplay,
-                        fontWeight: FontWeight.w900,
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-            ),
-          ],
-        ),
+      body: CustomCategoryCardHomeScreen(
+        title: 'Art',
+        image: 'assets/images/category_image1.png',
       ),
     );
   }
--- lib/features/home/widgets/custom_category_home_screen.dart
+++ lib/features/home/widgets/custom_category_home_screen.dart
@@ -1,45 +1,62 @@
-// import 'dart:ui';
-// import 'package:flutter/material.dart';
+import 'dart:ui';
+import 'package:flutter/material.dart';
+import 'package:mini_nft_marketplace_with_modern_ui/core/resources/fonts_manager.dart';
 
-// class CustomCategoryHomePage extends StatelessWidget {
-//   const CustomCategoryHomePage({super.key, required this.title, required this.image});
-//   final String title;
-//   final String image;
-//   @override
-//   Widget build(BuildContext context) {
-//     return  Stack(
-//       children: [
-//         ClipRRect(
-//           borderRadius: BorderRadius.circular(26),
-//           child:   Image(
-//             image: AssetImage(image),
-//             height: 167,
-//             width: 252,
-//           ),
-//         ),
-//         Positioned(bottom: PositionedValues.po,child: ClipRRect(
-//           borderRadius: const BorderRadius.only(
-//             bottomLeft: Radius.circular(26),
-//             bottomRight: Radius.circular(26),
-//           ),
-//           child: BackdropFilter(
-//             filter:ImageFilter.blur(sigmaX: BlurValues.b3,sigmaY: BlurValues.b3) ,
-//             child: Container(
-//                 alignment: Alignment.center,
-//                 decoration: BoxDecoration(
-//                     gradient: LinearGradient(
-//                         begin: Alignment.topCenter,
-//                         end: Alignment.bottomCenter,
-//                         colors: [
-//                           ColorManager.kColorBlack.withOpacity(0),
-//                           ColorManager.kColorBlack.withOpacity(.45),
-//                         ])),
-//                 height: HeightValues.h45_06,
-//                 width: WidthValues.w252_26,
-//                 child: Text(title,style: const TextStyle(fontSize: FontSize.f19_82,fontFamily: FontMangers.sfRroDisplay,fontWeight: FontWeight.bold),)),
-//           ),
-//         ),)
-//       ],
-//     );
-//   }
-// }
+class CustomCategoryCardHomeScreen extends StatelessWidget {
+  const CustomCategoryCardHomeScreen({
+    super.key,
+    required this.title,
+    required this.image,
+  });
+  final String title;
+  final String image;
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(26),
+      child: Stack(
+        children: [
+          // Background image
+          Image.asset(image, width: 280, height: 180, fit: BoxFit.cover),
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: ClipRRect(
+              borderRadius: BorderRadiusGeometry.only(
+                topLeft: Radius.circular(8),
+                topRight: Radius.circular(8),
+              ),
+              child: BackdropFilter(
+                filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+                child: Container(
+                  height: 55,
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                      colors: [
+                        Colors.transparent,
+                        Colors.transparent.withOpacity(0.3),
+                      ],
+                      stops: [0, 1],
+                    ),
+                  ),
+                  alignment: Alignment.center,
+                  child: Text(
+                    title,
+                    style: const TextStyle(
+                      fontSize: 20,
+                      fontFamily: FontsManager.sfProDisplay,
+                      fontWeight: FontWeight.w900,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
